0e7231d9314279bec129a1e3bceeaf43a7188282
coding-nerfilio <frisso2001@gmail.com>
2021-04-19 11:30:52 -0300

    Modified ChartInputs

--- src/ChartEditor/ChartInputs/index.tsx
+++ src/ChartEditor/ChartInputs/index.tsx
@@ -3,7 +3,12 @@ import "./style.scss";
 
 const ChartInputs = () => {
 	const InputComponent = useSelectInput();
-	return <div className="chart-inputs">{InputComponent}</div>;
+	return (
+		<div className="chart-inputs">
+			<div style={{ marginTop: 5, marginBottom: 5 }}>Datasets:</div>
+			{InputComponent}
+		</div>
+	);
 };
 
 export default ChartInputs;
