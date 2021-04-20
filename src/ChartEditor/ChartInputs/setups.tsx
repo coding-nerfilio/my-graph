9f80689c7e55777914eb68a057bfe5367327b90c
coding-nerfilio <frisso2001@gmail.com>
2021-04-20 10:37:14 -0300

    Added ScatterChart

--- src/ChartEditor/ChartInputs/setups.tsx
+++ src/ChartEditor/ChartInputs/setups.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import Button from "../../shared/components/Button";
 import DataSet1Input from "../../shared/components/DataSetInput/DataSet1";
 import DataSet2Input from "../../shared/components/DataSetInput/DataSet2";
+import DataSet4Input from "../../shared/components/DataSetInput/DataSet4";
 import { Context } from "../../shared/store";
 
 import Actions, { Action } from "../../shared/store/actions";
@@ -18,6 +19,22 @@ export const PlotInputs = () => {
 			</Button>
 			{state.dataSet1.map((dataset, index) => (
 				<DataSet1Input data={dataset} dispatcher={dispatcher} index={index} />
+			))}
+		</>
+	);
+};
+
+export const ScatterInputs = () => {
+	const [state, dispatcher] = useContext<[State, (action: Action) => {}]>(
+		Context
+	);
+	return (
+		<>
+			<Button onClick={() => dispatcher(Actions.dataSet4.add())}>
+				Add dataset
+			</Button>
+			{state.dataSet4.map((dataset, index) => (
+				<DataSet4Input data={dataset} dispatcher={dispatcher} index={index} />
 			))}
 		</>
 	);
--- src/shared/hooks/useSelectChart.tsx
+++ src/shared/hooks/useSelectChart.tsx
@@ -14,7 +14,7 @@ const useSelectChart = () => {
 				setChart(<Line data={ChartAdapters.Line(state.dataSet1)} />);
 				break;
 			case 1:
-				setChart(<Scatter data={ChartAdapters.ScatterPlot} />);
+				setChart(<Scatter data={ChartAdapters.ScatterPlot(state.dataSet4)} />);
 				break;
 			case 2:
 				setChart(
@@ -43,101 +43,15 @@ const useSelectChart = () => {
 				setChart(<Doughnut data={ChartAdapters.Doughnut(state.dataSet2)} />);
 				break;
 		}
-	}, [state.selectedChart, state.dataSet1, state.dataSet2, state.dataSet3]);
+	}, [
+		state.selectedChart,
+		state.dataSet1,
+		state.dataSet2,
+		state.dataSet3,
+		state.dataSet4,
+	]);
 
 	return chart;
 };
 
 export default useSelectChart;
-
-/*
-<Scatter
-							data={{
-								datasets: [
-									{
-										label: "Scatter Dataset",
-										data: [
-											{
-												x: -10,
-												y: 0,
-											},
-											{
-												x: 0,
-												y: 10,
-											},
-											{
-												x: 10,
-												y: 5,
-											},
-											{
-												x: 0.5,
-												y: 5.5,
-											},
-										],
-										backgroundColor: "rgb(255, 99, 132)",
-									},
-									{
-										label: "Scatter Dset",
-										data: [
-											{
-												x: -10,
-												y: 5,
-											},
-											{
-												x: 0,
-												y: 1,
-											},
-											{
-												x: 6,
-												y: 1,
-											},
-											{
-												x: 5,
-												y: 5,
-											},
-										],
-										backgroundColor: "rgb(0, 99, 132)",
-									},
-								],
-							}}
-						/>
-
-<Bar
-							data={{
-								labels: [
-									"January",
-									"February",
-									"March",
-									"April",
-									"May",
-									"June",
-									"July",
-								],
-								datasets: [
-									{
-										label: "My First Dataset",
-										data: [65, 59, 80, 81, 56, 55, 40],
-										backgroundColor: [
-											"rgba(255, 99, 132, 0.2)",
-											"rgba(255, 159, 64, 0.2)",
-											"rgba(255, 205, 86, 0.2)",
-											"rgba(75, 192, 192, 0.2)",
-											"rgba(54, 162, 235, 0.2)",
-											"rgba(153, 102, 255, 0.2)",
-											"rgba(201, 203, 207, 0.2)",
-										],
-										borderColor: [
-											"rgb(255, 99, 132)",
-											"rgb(255, 159, 64)",
-											"rgb(255, 205, 86)",
-											"rgb(75, 192, 192)",
-											"rgb(54, 162, 235)",
-											"rgb(153, 102, 255)",
-											"rgb(201, 203, 207)",
-										],
-										borderWidth: 1,
-									},
-								],
-							}}
-						/>
-						*/
