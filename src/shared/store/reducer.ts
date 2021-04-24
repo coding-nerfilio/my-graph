5c7b85afa094ffc6bb47cddd860ef537e73eeae1
coding-nerfilio <frisso2001@gmail.com>
2021-04-24 14:35:28 -0300

    Added DataSet5's reducer to Root reducer

--- src/shared/store/reducer.ts
+++ src/shared/store/reducer.ts
@@ -4,6 +4,7 @@ import dataSet1 from "./dataSet1/reducer";
 import dataSet2 from "./dataSet2/reducer";
 import dataSet3 from "./dataSet3/reducer";
 import dataSet4 from "./dataSet4/reducer";
+import dataSet5 from "./dataSet5/reducer";
 import { State } from "../../types/store/state";
 import { Action, EAction } from "../../types/store/actions";
 
@@ -46,6 +47,14 @@ const RootReducer = (state: State, action: Action): State => {
 		case EAction.DATASET4_SET_COLOR:
 			return dataSet4(state, action);
 
+		case EAction.DATASET5_CLEAR:
+		case EAction.DATASET5_ADD:
+		case EAction.DATASET5_REMOVE:
+		case EAction.DATASET5_SET_RAW:
+		case EAction.DATASET5_SET_COLOR:
+		case EAction.DATASET5_SET_LABEL:
+		case EAction.DATASET5_SET_TITLE:
+			return dataSet5(state, action);
 		default:
 			return state;
 	}
